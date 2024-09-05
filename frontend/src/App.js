21ea8687567ee6f6e93c8bd60b1d53a156ffcb04
Rijkaard Melo <rijkaardmelo@gmail.com>
2024-09-05 00:30:22 -0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import SendIcon from '@mui/icons-material/Send';
+import axios from 'axios';
 
 function App() {
   const [file, setFile] = useState(null);
+  const [type, setType] = useState(null);
+  const [item, setItem] = useState([]);
+  const [selectedItem, setSelectedItem] = useState([]);
+  const [message, setMessage] = useState('');
 
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
-  };
+  useEffect(() => {
+    const fetchItem = async () => {
+      try {
+        const response = await axios.get('/file');
+        setItem(response.data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchItem();
+  }, []);
 
   const handleUpload = () => {
-    
     const formData = new FormData();
     formData.append('file', file);
 
@@ -17,16 +37,96 @@ function App() {
       body: formData,
     })
       .then(response => response.json())
+      .then(data => {
+        window.location.reload();
+      })
       .catch(error => {
         console.error('Error:', error);
       });
   };
 
+  const handleText = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('/send_message', { message });
+      console.log('Resposta da API:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleItem = async (event) => {
+    event.preventDefault();
+    var response = null
+    try {
+      if (type === 'document')
+        response = await axios.post('/send_document', { selectedItem });
+      else if (type === 'media')
+        response = await axios.post('/send_media', { selectedItem });
+      console.log('Resposta da API:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Upload a File</h1>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
+      <div>
+        <form onSubmit={handleText}>
+          <label>Mensagem</label>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Multiline"
+            value={message}
+            multiline
+            maxRows={4}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant="contained" type="submit" endIcon={<SendIcon />}> Enviar </Button>
+        </form>
+      </div>
+      <div>
+        <label>Importar Arquivo</label>
+        <Button variant="contained" component="label">
+          Upload
+          <input type="file" hidden onChange={(e) => setFile(e.target.files[0])} />
+        </Button>
+        {file && (
+          <label>{file.name} </label>
+        )}
+        <Button variant="contained" onClick={handleUpload} endIcon={<SendIcon />}> Enviar </Button>
+      </div>
+      <div>
+        <label>Enviar Arquivo</label>
+        <FormControl component="fieldset">
+          <RadioGroup
+            aria-label="type"
+            defaultValue="document"
+            name="type"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          >
+            <FormControlLabel value="document" control={<Radio />} label="Documento" />
+            <FormControlLabel value="media" control={<Radio />} label="Fotos e vídeos" />
+          </RadioGroup>
+          <RadioGroup
+            aria-label="items"
+            name="items"
+            value={selectedItem}
+            onChange={(e) => setSelectedItem(e.target.value)}
+          >
+            {item.map((item, index) => (
+              <FormControlLabel
+                key={index}
+                value={item.fileaddress}
+                control={<Radio />}
+                label={item.filename}
+              />
+            ))}
+          </RadioGroup>
+          <Button variant="contained" onClick={handleItem} endIcon={<SendIcon />}>Enviar</Button>
+        </FormControl>
+      </div>
     </div>
   );
 }
--- main.py
+++ main.py
@@ -10,23 +10,23 @@ import os
 UPLOAD_FOLDER = 'upload'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-app = Flask(__name__, static_folder='frontend/build/staticy', template_folder='frontend/build')
+app = Flask(__name__, static_folder='frontend/build/static', template_folder='frontend/build')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# def is_connected():
-#     time = 0
-#     while time < 60:
-#         try:
-#             if driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/header/div/div/div/div/span/div/div[2]/div[2]/div/div/div/div/img'):
-#                 return True
-#         except:
-#             time +=1
-#             sleep(1)
-#     return False
+def is_connected():
+    time = 0
+    while time < 60:
+        try:
+            if driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/header/div/div/div/div/span/div/div[2]/div[2]/div/div/div/div/img'):
+                return True
+        except:
+            time +=1
+            sleep(1)
+    return False
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -46,69 +46,78 @@ def list_file():
     for filename in os.listdir(UPLOAD_FOLDER):
         fileaddress = os.path.join(UPLOAD_FOLDER, filename)
         if(os.path.isfile(fileaddress)):
-            files.append(filename)
+            files.append({
+                'filename': filename,
+                'fileaddress': path + "\\" + UPLOAD_FOLDER + "\\" + filename
+                })
     return jsonify(files)
 
+@app.route("/send_message", methods=["POST"])
+def send_message():
+    message = request.get_json()['message']
+    if(is_connected()):
+            try:
+                for contato in contatos.values:
+                    link = f'https://web.whatsapp.com/send?phone={contato[1]}&text={message}'
+                    driver.get(link)
+                    while len(driver.find_elements(By.ID, 'side')) < 1:
+                        sleep(1)
+                    sleep(5)
+                    driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span').click()
+                    sleep(5)
+            except:
+                return make_response(jsonify('Erro enviar'), 400)
+    else:
+        return make_response(jsonify("Erro conectar"), 400)
+    return make_response(jsonify('Enviado'), 200)
 
-# @app.route("/send_message", methods=["GET"])
-# def send_message():
-#     if(is_connected()):
-#         for contato in contatos.values:
-#             try:
-#                 link = f'https://web.whatsapp.com/send?phone={contato[1]}&text=Olá'
-#                 driver.get(link)
-#                 while len(driver.find_elements(By.ID, 'side')) < 1:
-#                     sleep(1)
-#                 sleep(5)
-#                 driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span').click()
-#                 sleep(5)
-#                 return make_response(jsonify('Enviado'), 200)
-#             except:
-#                 return make_response(jsonify('Erro enviar'), 400)
-#     else:
-#         return make_response(jsonify("Erro conectar"), 400)
+@app.route("/send_document", methods=["POST"])
+def send_document():
+    address = request.get_json()['selectedItem']
+    try:
+        for contato in contatos.values:
+            link = f'https://web.whatsapp.com/send?phone={contato[1]}'
+            driver.get(link)
+            while len(driver.find_elements(By.ID, 'side')) < 1:
+                sleep(1)
+            sleep(2)
+            driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div/div/span').click()
+            sleep(2)
+            driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[1]/li/div/span').click()
+            sleep(2)
+            keyboard = Controller()
+            keyboard.type(address)
+            keyboard.press(Key.enter)
+            sleep(2)
+            # driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/div/div/div[2]/div/div[2]/div[2]/div/div/span').click()
+            # sleep(5)
+    except:
+        return make_response(jsonify('Erro enviar'), 400)
+    return make_response(jsonify('Enviado'), 200)
 
-# def send_document():
-#     for contato in contatos.values:
-#         try:
-#             link = f'https://web.whatsapp.com/send?phone={contato[1]}'
-#             driver.get(link)
-#             while len(driver.find_elements(By.ID, 'side')) < 1:
-#                 sleep(1)
-#             sleep(2)
-#             driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div/div/span').click()
-#             sleep(2)
-#             driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[1]/li/div/span').click()
-#             sleep(2)
-#             keyboard = Controller()
-#             keyboard.type(r"C:\Users\Rijkaard Melo\Documents\chatbot\file.pdf")
-#             keyboard.press(Key.enter)
-#             sleep(2)
-#             driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/div/div/div[2]/div/div[2]/div[2]/div/div/span').click()
-#             sleep(5)
-#         except:
-#             print(f'Não foi possível enviar documento para {contato[0]}')
-
-# def send_image():
-#     for contato in contatos.values:
-#         try:
-#             link = f'https://web.whatsapp.com/send?phone={contato[1]}'
-#             driver.get(link)
-#             while len(driver.find_elements(By.ID, 'side')) < 1:
-#                 sleep(1)
-#             sleep(2)
-#             driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div/div/span').click()
-#             sleep(2)
-#             driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[2]/li/div/span').click()
-#             sleep(2)
-#             keyboard = Controller()
-#             keyboard.type(r"C:\Users\Rijkaard Melo\Documents\chatbot\flamengo.jpg")
-#             keyboard.press(Key.enter)
-#             sleep(2)
-#             driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/div/div/div[2]/div/div[2]/div[2]/div/div/span').click()
-#             sleep(5)
-#         except:
-#             print(f'Não foi possível enviar documento para {contato[0]}')
+@app.route("/send_media", methods=["POST"])
+def send_media():
+    address = request.get_json()['selectedItem']
+    try:
+        for contato in contatos.values:
+            link = f'https://web.whatsapp.com/send?phone={contato[1]}'
+            driver.get(link)
+            while len(driver.find_elements(By.ID, 'side')) < 1:
+                sleep(1)
+            sleep(2)
+            driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div/div/span').click()
+            sleep(2)
+            driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[2]/li/div/span').click()
+            sleep(2)
+            keyboard = Controller()
+            keyboard.type(address)
+            keyboard.press(Key.enter)
+            sleep(2)
+            # driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/div/div/div[2]/div/div[2]/div[2]/div/div/span').click()
+            # sleep(5)
+    except:
+        return make_response(jsonify('Erro enviar'), 400)
+    return make_response(jsonify('Enviado'), 200)
 
 @app.route('/', methods=['GET'])  
 def index():
@@ -116,16 +125,16 @@ def index():
     
 if __name__ == '__main__':
 
-    # contatos = pandas.read_csv("Contatos.csv")
+    contatos = pandas.read_csv("Contatos.csv")
 
-    # path = os.getcwd()
-    # profile = os.path.join(path, "whatsapp")
+    path = os.getcwd()
+    profile = os.path.join(path, "whatsapp")
 
-    # options = webdriver.ChromeOptions()
-    # options.add_argument(r"user-data-dir={}".format(profile))
+    options = webdriver.ChromeOptions()
+    options.add_argument(r"user-data-dir={}".format(profile))
 
-    # driver = webdriver.Chrome(options=options)
-    # driver.get("https://web.whatsapp.com/")
-    # driver.maximize_window()
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://web.whatsapp.com/")
+    driver.maximize_window()
 
     app.run()
